979125dc7107a8b21af5aab2863c88b421d00e50
Yashmori09 <100696404+Yashmori09@users.noreply.github.com>
2023-05-09 15:55:02 +0530

    Update 1

--- region_interest.py
+++ region_interest.py
@@ -1,18 +1,46 @@
 import cv2
 import numpy as np
+from PIL import Image
+import os
+img=cv2.imread("images\original.jpg")
+imag = Image.open("images\original.jpg")
+# r,thresh1 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+# # adap=cv2.adaptiveThreshold(img, 192,cv2.THRESH_BINARY,cv2.ADAPTIVE_THRESH_MEAN_C,11,12)
 
-img=cv2.imread("images\\5.jpg")
-img=cv2.resize(img,(512,512))
-img2=cv2.imread("images\\1.jpg")
-img2=cv2.resize(img2,(512,512))
-r=cv2.selectROI("select the area", img2)
-print(r)
-cv2.imshow('or operation', r)
-cv2.imshow('orginal', img2)
-cropped_image = img2[int(r[1]):int(r[1]+r[3]), 
-                      int(r[0]):int(r[0]+r[2])]
-cv2.imshow('and operation', cropped_image)
+# cv2.imshow('Threshold', thresh1)
+# cv2.imshow('Org',img)
+# # cv2.imshow('Adap',adap)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# import cv2
+# import numpy as np
 
+# dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+# img=cv2.imread("/content/emotions/images/images/train/angry/0.jpg")
+  # noice=cv2.add(image_res,gauss_noise)
+face_detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faces = face_detector.detectMultiScale(img, 1.3, 5)
+print(faces[0])
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    img2 = face = img[y:y + h, x:x + w]
 
+cv2.imwrite('detected..jpg', img)
+cv2.imwrite('croped.jpg', img2)
+graying = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('graying.jpg',graying)
+# thresh1 =cv2.threshold(histo,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+kernel = np.ones((5,5),np.uint8)
+erosion = cv2.erode(graying,kernel,iterations = 1)
+cv2.imwrite('erosion.jpg', erosion)
+dilation = cv2.dilate(erosion,kernel,iterations = 1)
+cv2.imwrite('diluted.jpg', dilation)
+histo=cv2.equalizeHist(dilation)
+cv2.imwrite('histo.jpg', histo)
+cv2.imshow('Threshold', histo)
+cv2.imshow('Org',img)
+cv2.imshow('img',img)
+cv2.imshow('crop',img2)
+# cv2.imshow('Adap',adap)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
